242d2c270a43e46bdd8faffa73689f0078a970a6
blackpanther409 <41005814+blackpanther409@users.noreply.github.com>
2018-10-06 12:47:59 +0530

    Update list_ADT.c

--- list_ADT.c
+++ list_ADT.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 
 struct list
-{ int a[20];
-  int lsize;
+{ 	int a[20];
+  	int lsize;
 };
 void insert( struct list *pl,int pos,int x);
 void print(struct list l);
@@ -12,70 +12,68 @@ void isEmpty(struct list l);
 void isFull(struct list l);
 
 int main()
-{ int pos,x;
-  int c;
-  struct list l;
-  l.lsize=0;
-  A:
-  printf("enter 1 to insert \n 2 to delete\n 3 to search \n 4 to check is_empty? \n 5 to check is_full?\n");
-  fflush(stdin);
-  scanf("%d",&c);
-  if(c==1)
-  { printf("Enter position and value to be inserted\n");
-    fflush(stdin);
-    scanf("%d%d",&pos,&x);
-    if(pos>=0 && pos<20 && pos<=(l.lsize))
-    {insert(&l,pos,x);
-     print(l);}
-    else
-    printf("Invalid position to insert into the array please try again\n");goto A;
-  }
+{	int pos,x;
+  	int c;
+  	struct list l;
+  	l.lsize=0;
+  	A:
+  	printf("enter 1 to insert \n 2 to delete\n 3 to search \n 4 to check is_empty? \n 5 to check is_full?\n");
+  	fflush(stdin);
+  	scanf("%d",&c);
+  	if(c==1)
+  		{ printf("Enter position and value to be inserted\n");
+    	  fflush(stdin);
+    	  scanf("%d%d",&pos,&x);
+    	  if(pos>=0 && pos<20 && pos<=(l.lsize))
+    		{ insert(&l,pos,x);
+     		  print(l);}
+    	  else
+    		printf("Invalid position to insert into the array please try again\n");goto A;
+  		}
 
-  else if(c==2)
-  { if(l.lsize>0)
-     { printf("Enter the position of the array to be deleted\n");
-       scanf("%d",&pos);
-       if(pos>=0 && pos<=20 && pos<l.lsize)
-      { delete(&l,pos);
-       print(l);}
-       else
-       printf("deleting is not possible ,please try again\n");goto A;
-     }
-       else
+  	else if(c==2)
+  		{ if(l.lsize>0)
+     		{ printf("Enter the position of the array to be deleted\n");
+       		  scanf("%d",&pos);
+       	  if(pos>=0 && pos<=20 && pos<l.lsize)
+      		{ delete(&l,pos);
+       		  print(l);}
+       	  else
+       		printf("deleting is not possible ,please try again\n");goto A;
+     		}
+     else
        printf("deleting is not possible as array is empty ,please try again\n");goto A;
-  }
+  		}
   else if(c==3)
-  { if(l.lsize>0)
-    { printf("enter value to be searched\n");
-      scanf("%d",&x);
-      search(&l,x);
-    }
-    else
-    printf("searching is not possible as array is empty ,please try again\n"); goto A;
-  }
+ 	 { if(l.lsize>0)
+    	{ printf("enter value to be searched\n");
+      	  scanf("%d",&x);
+          search(&l,x);
+  	    }
+  	   else
+    	printf("searching is not possible as array is empty ,please try again\n"); goto A;
+  	 }
   else if(c==4)
-  { isEmpty(l);goto A;
-  }
+  	  isEmpty(l);goto A;
   else if(c==5)
-  { isFull(l);goto A;
-  }
-return 0;
+  	{   isFull(l);
+    	goto A;
+  	}
+  return 0;
 }
 void insert( struct list *pl,int pos,int x)
 {
 
   int i;
   for(i=pl->lsize;i>=pos;i--)
-  {pl->a[i+1]=pl->a[i];}
+  	 pl->a[i+1]=pl->a[i];
   pl->a[pos]=x;
   pl->lsize++;
-
-
 }
 
 void delete( struct list *pl,int pos)
 { for(int i=pos;i<(pl->lsize)-1;i++)
-  pl->a[i]=pl->a[i+1];
+  	pl->a[i]=pl->a[i+1];
   pl->lsize--;
 
 }
@@ -83,20 +81,19 @@ void delete( struct list *pl,int pos)
 void print(struct list l)
 {
   for(int i=0;i<l.lsize;i++)
-  printf("%d  ",l.a[i]);
+  	printf("%d  ",l.a[i]);
   printf("\n");
 }
 
 void search( struct list *pl,int x)
 { int k=0;
   for(int i=0;i<(pl->lsize);i++)
-   {if(pl->a[i]==x)
-    k=k+1;}
+   	{if(pl->a[i]==x)
+     k=k+1;}
   if(k>0)
-  printf("the element is present in the array\n");
+  	printf("the element is present in the array\n");
   else
-  printf("the element is not present in the array\n");
-
+  	printf("the element is not present in the array\n");
 }
 
 void isEmpty(struct list l)
